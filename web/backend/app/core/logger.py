7498aecf84ad1e31de85d125b200489c072d6333
robert <robertwilliam0963@gmail.com>
2026-03-05 21:28:55 -0300

    Ajustes de definição do projeto

--- web/backend/app/core/logger.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-import sys
-
-logger.remove()
-logger.add(sys.stdout, level="INFO")
-
-def get_logger():
-    return logger
--- web/backend/app/routes/bootstrap.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.services.argocd_service import install_argocd
-
-router = APIRouter(prefix="/bootstrap", tags=["Bootstrap"])
-
-@router.post("/argocd")
-def bootstrap_argocd():
-    return install_argocd()
